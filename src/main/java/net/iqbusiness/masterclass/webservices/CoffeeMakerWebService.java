4922e912736a58ea2d481620414a5b0ffb39ceef
Ajit Bawa <ajbawa@gmail.com>
2017-04-23 20:48:46 +0200

    Changed Logging format of Requests and Responses.

--- src/main/java/net/iqbusiness/masterclass/webservices/CoffeeMakerWebService.java
+++ src/main/java/net/iqbusiness/masterclass/webservices/CoffeeMakerWebService.java
@@ -2,6 +2,8 @@ package net.iqbusiness.masterclass.webservices;
 
 import net.iqbusiness.masterclass.model.Coffee;
 import net.iqbusiness.masterclass.repository.CoffeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,11 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/coffee")
 public class CoffeeMakerWebService {
 
+    private static final Logger log = LoggerFactory.getLogger(CoffeeMakerWebService.class);
+
     @Autowired
     private CoffeeRepository coffeeRepository;
 
     @RequestMapping(method = RequestMethod.GET)
     public Coffee makeCoffee() {
-            return coffeeRepository.getCoffee();
+        log.info("\n\n\t>>> Request to makeCoffee() >>>\n");
+        Coffee coffee = coffeeRepository.getCoffee();
+        log.info("\n\n\t<<< Response to makeCoffee(): {} <<<\n", coffee);
+        return coffee;
     }
 }
